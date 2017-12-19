d5085821b1bfecfcab243972e9fbeab655b22a12
boypro477 <nguyenanhtu0407@gmail.com>
2017-12-19 15:58:04 +0700

    speech

--- models/contentModel.js
+++ models/contentModel.js
@@ -2,8 +2,8 @@ const mongoose=require('mongoose');
 const Schema=mongoose.Schema;
 
 const contentModel=new Schema({
-
-    content:{type:String,require:true,unique:true},
     keyword:{type:String,require:true,unique:true},
+    content:{type:String,require:true},
+
 });
 module.exports=mongoose.model('content',contentModel)
