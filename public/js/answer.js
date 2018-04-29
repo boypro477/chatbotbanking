8b991ebb887e8c3eb4add8f1b9220540cfeb9b4c
boypro477 <nguyenanhtu0407@gmail.com>
2018-04-29 19:31:14 +0700

    1.10

--- public/js/answer.js
+++ public/js/answer.js
@@ -6,17 +6,14 @@ $("#text_form").on("submit", event => {
     data: $("#text_form").serialize()
   })
     .then(answer => {
-      // console.log(answer);
-      // var result=answer;
-      // $("#feedback").text(answer.answer);
       document.getElementById('output').innerHTML+='<div class="guest">'+'<p>'+'<b>'+'Guest: '+'</b>'+document.getElementById('Textarea').value+'</br'+'</p>'+"</div>";      
       document.getElementById("output").innerHTML+='<div class="bot">'+'<p>'+'<b>'+'BOT: '+'</b>'+answer.answer+'</br>'+'</p>'+'</div>';
       document.getElementById('Textarea').value='';
       var text= answer.answer;
-      text = encodeURIComponent(text);
+      // text = encodeURIComponent(text);
       var url = "https://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q="+text+"&tl=vi&total=1&idx=0&textlen=3";
-      // $('audio').attr('src',url).get(0).play();
-      $('#audio').html('<audio autoplay><source src="'+url+'"></audio>');
+      $('audio').attr('src',url).get(0).play();
+      // $('#audio').html('<audio autoplay><source src="'+url+'"></audio>');
     })
     .fail(err => {
       alert("An error occured, please try again later");
